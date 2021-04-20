269c44596d686deb4b600afaa43c67ddc3ef2a74
Snaksiu <72550913+Snaksiu@users.noreply.github.com>
2021-04-20 13:47:25 +0200

    REMISU TYLKO NIE MA BO NIE
    TAK O

--- TICTACTOE/Form1.cs
+++ TICTACTOE/Form1.cs
@@ -51,6 +51,10 @@ namespace TICTACTOE
         private void Mark(object sender, EventArgs e)
         {
             Button senderButton = (Button)sender;
+            if(senderButton.Text.Contains("X") || senderButton.Text.Contains("O"))
+            {
+                return;
+            }
             if(cp == CurrentPlayer.Cross)
             {
                 senderButton.Text = "X";
@@ -61,7 +65,11 @@ namespace TICTACTOE
                 senderButton.Text = "O";
                 cp = CurrentPlayer.Cross;
             }
+            checkForWinner();
             changeLabel();
+
+
+
         }
         public void changeLabel()
         {
@@ -73,6 +81,118 @@ namespace TICTACTOE
             {
                 PlayerLabel.Text = "Kółko";
             }
+        }
+        public void checkForWinner()
+        {
+            if (tl.Text.Contains("X") && cl.Text.Contains("X") && bl.Text.Contains("X"))
+            {
+                Form2 victoryScreen = new Form2(this);
+                victoryScreen.winner = tl.Text;
+                victoryScreen.Show();
+            }
+            else if (tc.Text.Contains("X") && cc.Text.Contains("X") && bc.Text.Contains("X"))
+            {
+                Form2 victoryScreen = new Form2(this);
+                victoryScreen.winner = tc.Text;
+                victoryScreen.Show();
+            }
+            else if (tr.Text.Contains("X") && cr.Text.Contains("X") && br.Text.Contains("X"))
+            {
+                Form2 victoryScreen = new Form2(this);
+                victoryScreen.winner = tr.Text;
+                victoryScreen.Show();
+            }
+            else if (tl.Text.Contains("X") && tc.Text.Contains("X") && tr.Text.Contains("X"))
+            {
+                Form2 victoryScreen = new Form2(this);
+                victoryScreen.winner = tl.Text;
+                victoryScreen.Show();
+            }
+            else if (cl.Text.Contains("X") && cc.Text.Contains("X") && cr.Text.Contains("X"))
+            {
+                Form2 victoryScreen = new Form2(this);
+                victoryScreen.winner = tr.Text;
+                victoryScreen.Show();
+            }
+            else if (bl.Text.Contains("X") && bc.Text.Contains("X") && br.Text.Contains("X"))
+            {
+                Form2 victoryScreen = new Form2(this);
+                victoryScreen.winner = bl.Text;
+                victoryScreen.Show();
+            }
+            else if (tl.Text.Contains("X") && cc.Text.Contains("X") && br.Text.Contains("X"))
+            {
+                Form2 victoryScreen = new Form2(this);
+                victoryScreen.winner = tl.Text;
+                victoryScreen.Show();
+            }
+            else if (tr.Text.Contains("X") && cc.Text.Contains("X") && bl.Text.Contains("X"))
+            {
+                Form2 victoryScreen = new Form2(this);
+                victoryScreen.winner = tr.Text;
+                victoryScreen.Show();
+            }
+            else if (tl.Text.Contains("O") && cl.Text.Contains("O") && bl.Text.Contains("O"))
+            {
+                Form2 victoryScreen = new Form2(this);
+                victoryScreen.winner = tl.Text;
+                victoryScreen.Show();
+            }
+            else if (tc.Text.Contains("O") && cc.Text.Contains("O") && bc.Text.Contains("O"))
+            {
+                Form2 victoryScreen = new Form2(this);
+                victoryScreen.winner = tc.Text;
+                victoryScreen.Show();
+            }
+            else if (tr.Text.Contains("O") && cr.Text.Contains("O") && br.Text.Contains("O"))
+            {
+                Form2 victoryScreen = new Form2(this);
+                victoryScreen.winner = tr.Text;
+                victoryScreen.Show();
+            }
+            else if (tl.Text.Contains("O") && tc.Text.Contains("O") && tr.Text.Contains("O"))
+            {
+                Form2 victoryScreen = new Form2(this);
+                victoryScreen.winner = tl.Text;
+                victoryScreen.Show();
+            }
+            else if (cl.Text.Contains("O") && cc.Text.Contains("O") && cr.Text.Contains("O"))
+            {
+                Form2 victoryScreen = new Form2(this);
+                victoryScreen.winner = tr.Text;
+                victoryScreen.Show();
+            }
+            else if (bl.Text.Contains("O") && bc.Text.Contains("O") && br.Text.Contains("O"))
+            {
+                Form2 victoryScreen = new Form2(this);
+                victoryScreen.winner = bl.Text;
+                victoryScreen.Show();
+            }
+            else if (tl.Text.Contains("O") && cc.Text.Contains("O") && br.Text.Contains("O"))
+            {
+                Form2 victoryScreen = new Form2(this);
+                victoryScreen.winner = tl.Text;
+                victoryScreen.Show();
+            }
+            else if (tr.Text.Contains("O") && cc.Text.Contains("O") && bl.Text.Contains("O"))
+            {
+                Form2 victoryScreen = new Form2(this);
+                victoryScreen.winner = tr.Text;
+                victoryScreen.Show();
+            }
+        }
+        public void clearBoard()
+        {
+            TableLayoutControlCollection buttons = tableLayoutPanel1.Controls;
+
+            for(int i = 0; i < buttons.Count; i++)
+            {
+                if (buttons[i] is Button)
+                    buttons[i].Text = "";  
+            }
+
+            cp = CurrentPlayer.Cross;
+            changeLabel();
         }
     }
 
